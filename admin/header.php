0597f07aef77297c2be5b504e9f8872475fb0f0c
datndph41182 <duydat3052004@gmail.com>
2023-11-20 15:02:45 +0700

    update ngay 20/11/2023

---
+++ admin/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="boxcenter">
+        <div class="row mb">
+            <div class="logo">
+                <img src="images/Ảnh chụp màn hình 2023-11-16 123804.png" alt="">
+                <h1>PERFUME DT</h1>
+            </div>
+        </div>
+        <div class="row mb menu">
+            <ul>
+                <li><a href="trangchu_admin.html">Trang chủ</a></li>
+                <li><a href="admin.html">Danh mục</a></li>
+                <li><a href="#">Sản phẩm</a></li>
+                <li><a href="#">Khách hàng</a></li>
+                <li><a href="#">Bình luận</a></li>
+                <li><a href="#">Thống kê</a></li>
+            </ul>
+        </div>
