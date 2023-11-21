ef0066dfc91d267aa6dc2c64b59733f0e3d143bb
datndph41182 <duydat3052004@gmail.com>
2023-11-21 12:10:07 +0700

    commit 21/11/2023

--- admin/home.php
+++ admin/home.php
@@ -1,23 +1,58 @@
 <div class="row">
-            <div class="row header">
-                <h1>THÊM MỚI LOẠI HÀNG HÓA</h1>
+            <div class="row">
+                <h1>Control panner</h1>
             </div>
-            <div class="row formcontent">
-                <form action="#" method="post">
-                    <div class="row mb10">
-                        Mã loại <br>
-                        <input type="text" name="maloai" disabled placeholder="auto number">
+                <div class="row mb">
+                    <div class="boxtitle">Sản phẩm mới</div>
+                    <div class="boxcontent"></div>
+                </div>
+                <div class="row mb">
+                    <div class="boxtitle">Sản phẩm xem nhiều</div>
+                    <div class="boxcontent"></div>
+                </div>
+                <div class="row mb">
+                    <div class="boxtitle">Bình luận mới</div>
+                    <div class="boxcontent"></div>
+                </div>
+                <div class="row mb">
+                    <div class="boxtitle">Đơn hàng mới</div>
+                    <div class="boxcontent"></div>
+                </div>
+                <div class="row mb">
+                    <div class="boxtitle">Biểu đồ thống kê sản phẩm theo danh mục</div>
+                    <div class="boxcontent">
+                        <canvas id="myChart"></canvas>
+
+    <script>
+        // Dữ liệu biểu đồ
+        var data = {
+            labels: ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6'],
+            datasets: [{
+                label: 'Doanh thu',
+                data: [500, 800, 600, 900, 700, 1000],
+                backgroundColor: 'rgba(75, 192, 192, 0.5)', // Màu nền cột
+                borderColor: 'rgba(75, 192, 192, 1)', // Màu viền cột
+                borderWidth: 1 // Độ dày viền cột
+            }]
+        };
+
+        // Cấu hình biểu đồ
+        var options = {
+            scales: {
+                y: {
+                    beginAtZero: true // Hiển thị từ 0 đến giá trị cao nhất
+                }
+            }
+        };
+
+        // Tạo biểu đồ cột
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: data,
+            options: options
+        });
+    </script>
                     </div>
-                    <div class="row mb10">
-                        Tên loại <br>
-                        <input type="text" name="tenloai">
-                    </div>
-                    <div class="row mb10">
-                        <input type="submit" value="Thêm mới">
-                        <input type="reset" value="Nhập lại">
-                        <a href="danhsach_loaihang.html"><input type="button" value="Danh sách"></a>
-                    </div>
-                </form>
+                </div>
             </div>
-        </div>
-    </div>
