6d54af68444eefa1c6abf5caa2fc8a9084470c22
tuanpaph <tuanpaph31233@fpt.edu.vn>
2023-12-08 20:44:00 +0700

    updategd

---
+++ duan2/view/home.php
@@ -0,0 +1,236 @@
+<!-- slider banner -->
+<section class="slider_section">
+      <div class="slider_container">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            
+            <div class="carousel-item active">
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-md-7">
+                    <div class="detail-box">
+                      <h1>Welcome To Our <br> Gift Shop</h1>
+                      <p>
+                        Sequi perspiciatis nulla reiciendis, rem, tenetur impedit, eveniet non necessitatibus error
+                        distinctio mollitia suscipit. Nostrum fugit doloribus consequatur distinctio esse, possimus
+                        maiores aliquid repellat beatae cum, perspiciatis enim, accusantium perferendis.
+                      </p>
+                      <a href="">Contact Us</a>
+                    </div>
+                  </div>
+                  <div class="col-md-5 ">
+                    <div class="img-box"><img src="images/slider-img.png" alt="" /></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="carousel-item ">
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-md-7">
+                    <div class="detail-box">
+                      <h1>Welcome To Our <br>Gift Shop</h1>
+                      <p>
+                        Sequi perspiciatis nulla reiciendis, rem, tenetur impedit, eveniet non necessitatibus error
+                        distinctio mollitia suscipit. Nostrum fugit doloribus consequatur distinctio esse, possimus
+                        maiores aliquid repellat beatae cum, perspiciatis enim, accusantium perferendis.
+                      </p>
+                      <a href="">Contact Us</a>
+                    </div>
+                  </div>
+                  <div class="col-md-5 ">
+                    <div class="img-box"><img src="images/slider-img.png" alt="" /></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="carousel-item ">
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-md-7">
+                    <div class="detail-box">
+                      <h1>Welcome To Our <br>Gift Shop</h1>
+                      <p>
+                        Sequi perspiciatis nulla reiciendis, rem, tenetur impedit, eveniet non necessitatibus error
+                        distinctio mollitia suscipit. Nostrum fugit doloribus consequatur distinctio esse, possimus
+                        maiores aliquid repellat beatae cum, perspiciatis enim, accusantium perferendis.
+                      </p>
+                      <a href="">Contact Us</a>
+                    </div>
+                  </div>
+                  <div class="col-md-5 ">
+                    <div class="img-box"><img src="images/slider-img.png" alt="" /></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+
+          <div class="carousel_btn-box">
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <i class="fa fa-arrow-left" aria-hidden="true"></i>
+              <span class="sr-only">Previous</span>
+            </a>
+            <img src="images/line.png" alt="" />
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <i class="fa fa-arrow-right" aria-hidden="true"></i>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+
+        </div>
+      </div>
+    </section>
+    <!-- end slider banner -->
+  </div>
+  <!-- end hero area -->
+
+  <!-- content -->
+  <section class="shop_section layout_padding">
+    <div class="container">
+
+      <div class="heading_container heading_center">
+        <h2>Latest Products</h2>
+      </div>
+      <div class="row">
+
+        <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="box">
+            <a href="chitietsanpham.html">
+              <div class="img-box">
+                <img src="images/Frame_4.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h6>Ring</h6>
+                <h6>Price<span>$200</span></h6>
+              </div>
+              <div class="new"><span>New</span></div>
+            </a>
+          </div>
+        </div>
+        
+        <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="images/Frame_2.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h6>Watch</h6>
+                <h6>Price<span> $300</span></h6>
+              </div>
+              <div class="new"><span>New</span></div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="images/Frame_3.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h6>Watch</h6>
+                <h6>Price<span> $300</span></h6>
+              </div>
+              <div class="new"><span>New</span></div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="images/Frame_5.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h6>Watch</h6>
+                <h6>Price<span> $300</span></h6>
+              </div>
+              <div class="new"><span>New</span></div>
+            </a>
+          </div>
+        </div>
+        
+        <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="images/Frame_6.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h6> Teddy Bear</h6>
+                <h6>Price<span>$95</span></h6>
+              </div>
+              <div class="new"><span>New</span></div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="images/Frame_7.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h6> Teddy Bear</h6>
+                <h6>Price<span>$95</span></h6>
+              </div>
+              <div class="new"><span>New</span></div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="images/Frame_8.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h6> Teddy Bear</h6>
+                <h6>Price<span>$95</span></h6>
+              </div>
+              <div class="new"><span>New</span></div>
+            </a>
+          </div>
+        </div>
+        
+        <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="images/Frame_4.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h6> Teddy Bear</h6>
+                <h6>Price<span>$95</span></h6>
+              </div>
+              <div class="new"><span>New</span></div>
+            </a>
+          </div>
+        </div>
+
+      </div>
+      <div class="btn-box">
+        <a href="">View All Products</a>
+      </div>
+    </div>
+  </section>
+  <!-- end content -->
+
+
+
+
+
+  <?php include "view/tietkiem.php" ?>
+  <?php include "view/muasam.php" ?>
+  <?php include "view/quatang.php" ?>
+  <?php include "view/lienhe.php" ?>
+  <?php include "view/chungthuc.php" ?>
---
+++ duan2/view/quatang.php
@@ -0,0 +1,33 @@
+  <!-- Hộp quà -->
+  <section class="gift_section layout_padding-bottom">
+    <div class="box ">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-5">
+            <div class="img_container">
+              <div class="img-box">
+                <img src="images/gifts.png" alt="">
+              </div>
+            </div>
+          </div>
+          <div class="col-md-7">
+            <div class="detail-box">
+              <div class="heading_container">
+                <h2>Quà tặng <br>Cho Những người yêu thương của bạn</h2>
+              </div>
+              <p>
+                Omnis ex nam laudantium odit illum harum, excepturi accusamus at corrupti, velit blanditiis unde
+                perspiciatis, vitae minus culpa? Beatae at aut consequuntur porro adipisci aliquam eaque iste ducimus
+                expedita accusantium?
+              </p>
+              <div class="btn-box">
+                <a href="#" class="btn1">Buy Now</a>
+                <a href="#" class="btn2">See More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end Hộp quà -->
